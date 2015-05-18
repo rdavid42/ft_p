50790203972fc9d70e4d83d1dcbf91afd775c2b8
Ryd <rdavid@student.42.fr>
2015-05-18 19:23:10 +0200

    pwd on the way

--- src/client/pwd.c
+++ src/client/pwd.c
@@ -6,13 +6,35 @@
 /*   By: rdavid <rdavid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/17 19:04:48 by rdavid            #+#    #+#             */
-/*   Updated: 2015/05/17 19:05:15 by rdavid           ###   ########.fr       */
+/*   Updated: 2015/05/18 19:21:51 by rdavid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+
+static int		check_recv(int r, int *sock)
+{
+	if (r == -1)
+		close(*sock), error(REC_ERR);
+	else if (!r)
+		close(*sock), error(CO_CLOSED);
+}
+
 int				pwd(int *sock, char *cmd)
 {
-	(void)sock;
-	(void)cmd;
+	int			pwd_size;
+	char		*pwd;
+	int			r;
+
+	pwd_size = 0;
+	r = recv(*sock, (void *)&pwd_size, sizeof(uint32_t), 0);
+	check_recv(r, sock);
+	pwd = (char *)malloc(sizeof(char) * pwd_size + 1);
+	r = recv(*sock, pwd, pwd_size, 0);
+	check_recv(r, sock);
+	(void)!write(1, pwd, pwd_size);
+	(void)!write(1, "\n", 1);
 	return (1);
 }
--- src/server/pwd.c
+++ src/server/pwd.c
@@ -6,14 +6,27 @@
 /*   By: rdavid <rdavid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/18 18:17:12 by rdavid            #+#    #+#             */
-/*   Updated: 2015/05/18 18:18:02 by rdavid           ###   ########.fr       */
+/*   Updated: 2015/05/18 19:22:34 by rdavid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int				pwd(char *root, int *cs, char *cmd)
+#include <unistd.h>
+#include "shared.h"
+
+int					pwd(char *root, int *cs, char *cmd)
 {
-	(void)root;
-	(void)cs;
+	char			buf[1024];
+	char			*cur = getcwd(buf, 1024);
+	int const		rlen = slen(root);
+	int const		clen = slen(cur);
+	int				ret;
+
 	(void)cmd;
+	if (!scmp(cur, root, slen(root)))
+	{
+		ret = clen - rlen;
+		send(*cs, (void *)&ret, sizeof(uint32_t), 0);
+		send(*cs, cur, ret, 0);
+	}
 	return (1);
 }
